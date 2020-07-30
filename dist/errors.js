4d1722f84106cafeaae5f6c1eac4eb85bb903a48
Gianni Moschini <gianni.moschini+github@gmail.com>
2020-07-30 11:49:54 +0100

    Build

--- dist/errors.js
+++ dist/errors.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.MissingAPIKeyError = void 0;
 class MissingAPIKeyError extends Error {
     constructor() {
         super("You must pass a valid API key.");
